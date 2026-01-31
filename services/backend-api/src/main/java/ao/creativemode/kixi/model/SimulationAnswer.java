a184645e23e54a604b04126b35eb6f1462d1c072
Obed jorge <Obedjorge22@gmail.com>
2026-01-31 23:37:51 +0100

    Entity

---
+++ services/backend-api/src/main/java/ao/creativemode/kixi/model/SimulationAnswer.java
@@ -0,0 +1,45 @@
+package ao.creativemode.kixi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.sql.Date;
+@Getter
+@Setter
+@Table("simulation_answer")
+public class SimulationAnswer {
+    @Id
+    private Long id;
+
+    @Column("simulation_id")
+    private Long simulationId;
+
+    @Column("question_id")
+    private Long questionId;
+
+    @Column("selected_option_id")
+    private Long selectedOptionId;
+
+    @Column("score_obtained")
+    private float scoreObtained;
+
+    @Column("is_correct")
+    private Boolean isCorrect;
+
+    @Column("answered_at")
+    private Date answeredAt;
+
+    @Column("created_at")
+    private Date createdAt;
+
+    @Column("updated_at")
+    private Date updatedAt;
+
+    @Column("deleted_at")
+    private Date deletedAt;
+
+}
+
