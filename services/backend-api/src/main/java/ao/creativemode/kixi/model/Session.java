a2171d319bfbc7e82e32be3e561d256a8cb5a5a7
Dizono Vundu <dizonovundu@gmail.com>
2026-01-31 11:55:37 +0100

    feat: Before doing some stupid changes

--- services/backend-api/src/main/java/ao/creativemode/kixi/model/Session.java
+++ services/backend-api/src/main/java/ao/creativemode/kixi/model/Session.java
@@ -1,6 +1,7 @@
 package ao.creativemode.kixi.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Transient;
