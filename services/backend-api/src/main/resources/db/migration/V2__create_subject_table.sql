b8ca521b2078c4c117de64a289cbac73ececd4f3
abnerLouren <abnerjaredejede@gmail.com>
2026-02-04 10:40:08 +0100

    feature: smt

--- services/backend-api/src/main/resources/db/migration/V2__create_subject_table.sql
+++ services/backend-api/src/main/resources/db/migration/V2__create_subject_table.sql
@@ -1,10 +1,14 @@
 CREATE TABLE subjects (
-    id          BIGSERIAL PRIMARY KEY ,
+    id          BIGSERIAL PRIMARY KEY,
     code        VARCHAR(50) NOT NULL,
     name        VARCHAR(255) NOT NULL,
-    short_name    VARCHAR(100) NOT NULL,
+    short_name  VARCHAR(100),
     created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     deleted_at  TIMESTAMP WITH TIME ZONE DEFAULT NULL,
 
-)
+    CONSTRAINT uk_subjects_code UNIQUE (code)
+);
+
+CREATE INDEX idx_subjects_code ON subjects(code);
+CREATE INDEX idx_subjects_active ON subjects(deleted_at) WHERE deleted_at IS NULL;
