1bd6f9dc973033238f82eae6e051c8292a4f9392
abnerLouren <abnerjaredejede@gmail.com>
2026-02-04 16:21:05 +0100

    feature: firt step paddle-ocr implementation

--- services/backend-api/src/main/java/ao/creativemode/kixi/model/Simulation.java
+++ services/backend-api/src/main/java/ao/creativemode/kixi/model/Simulation.java
@@ -1,10 +1,10 @@
 package ao.creativemode.kixi.model;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
-
-import java.time.LocalDateTime;
 
 @Table("simulation")
 public class Simulation {
